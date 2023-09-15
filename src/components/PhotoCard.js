3087565716a8b69ddce9c727b4f1e6aefc2afae2
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-15 23:40:28 +0100

    edit api

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,11 +1,21 @@
-// src/components/PhotoCard.js
 import React from "react";
 
 function PhotoCard({ photo }) {
+  if (
+    !photo ||
+    !photo.pagemap ||
+    !photo.pagemap.cse_image ||
+    !photo.pagemap.cse_image[0] ||
+    !photo.pagemap.cse_image[0].src ||
+    !photo.alt
+  ) {
+    return null;
+  }
+
   return (
     <div className="bg-white p-4 rounded shadow">
-      <img src={photo.urls.small} alt={photo.alt_description} />
-      <p className="text-gray-600 mt-2">Photo by {photo.user.name}</p>
+      <img src={photo.pagemap.cse_image[0].src} alt={photo.alt} />
+      <p className="text-gray-600 mt-2">Photo by {photo.title}</p>
     </div>
   );
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,37 +1,45 @@
 import React, { useState } from "react";
 import NavBar from "./NavBar";
 import PhotoCard from "./PhotoCard";
-import axios from "axios"; // Import Axios
-
+import axios from "axios";
 
 function SearchResults() {
   const [searchQuery, setSearchQuery] = useState("");
   const [results, setResults] = useState([]);
   const [error, setError] = useState(null);
 
-  const GOOGLE_API_KEY = "GOOGLE_API_KEY"; // Replace with your Google API key
-  const CX = "CUSTOM_SEARCH_ENGINE_ID"; // Replace with your Custom Search Engine ID
-
+  const GOOGLE_API_KEY = "AIzaSyBjuEER8jiiNtseBMMiRS8hth0Bfm-ziD8"; // Replace with your Google API key
+  const CX = "AIzaSyCf_kFGKYYj2fvIsgiUFQ7QvNcF316AuYE"; // Replace with your Custom Search Engine ID
 const handleSearch = async () => {
   try {
     setError(null); // Clear any previous errors
-    const response = await axios.get(
-      `https://www.googleapis.com/customsearch/v1?q=${searchQuery}&key=${GOOGLE_API_KEY}&cx=${CX}`
-    );
 
-    // Check if the response contains an 'items' array
-    if (response.data.items && Array.isArray(response.data.items)) {
-      // Extract image results from the data (modify this according to the API response structure)
-      const imageResults = response.data.items.filter(
-        (item) => item.pagemap && item.pagemap.cse_image
-      );
+    // Construct the API URL and log it
+    const apiUrl = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${CX}&q=${encodeURIComponent(
+      searchQuery
+    )}`;
+    console.log(apiUrl); // Log the API URL
 
-      setResults(imageResults);
+    // Make the API request
+    const response = await axios.get(apiUrl);
+
+    if (response.status === 200) {
+      // Check if the response contains an 'items' array
+      if (response.data.items && Array.isArray(response.data.items)) {
+        // Extract image results from the data (modify this according to the API response structure)
+        const imageResults = response.data.items.filter(
+          (item) => item.pagemap && item.pagemap.cse_image
+        );
+
+        setResults(imageResults);
+      } else {
+        throw new Error("Invalid response data format");
+      }
     } else {
-      throw new Error("Invalid response data format");
+      throw new Error(`Request failed with status code ${response.status}`);
     }
   } catch (error) {
-    console.error(error);
+    console.error("Error:", error);
     setError("An error occurred while fetching data. Please try again later.");
   }
 };
