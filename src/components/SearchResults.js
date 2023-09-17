d83b3e7c9c006953e71dfe89652a46a01cb81608
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-17 11:14:05 +0100

    fixing the bugs

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -8,42 +8,55 @@ function SearchResults() {
   const [results, setResults] = useState([]);
   const [error, setError] = useState(null);
 
-  const GOOGLE_API_KEY = "AIzaSyBjuEER8jiiNtseBMMiRS8hth0Bfm-ziD8"; // Replace with your Google API key
-  const CX = "AIzaSyCf_kFGKYYj2fvIsgiUFQ7QvNcF316AuYE"; // Replace with your Custom Search Engine ID
-const handleSearch = async () => {
-  try {
-    setError(null); // Clear any previous errors
+  const GOOGLE_API_KEY = process.env.REACT_APP_API_KEY;
+  const CX = process.env.REACT_APP_SEARCH_ENGINE_ID;
 
-    // Construct the API URL and log it
-    const apiUrl = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${CX}&q=${encodeURIComponent(
-      searchQuery
-    )}`;
-    console.log(apiUrl); // Log the API URL
+  const handleSearch = async () => {
+    try {
+      setError(null); // Clear any previous errors
 
-    // Make the API request
-    const response = await axios.get(apiUrl);
+      // Make the API request
+      const response = await axios.get(
+        "https://www.googleapis.com/customsearch/v1",
+        {
+          params: {
+            key: GOOGLE_API_KEY,
+            cx: CX,
+            q: encodeURIComponent(searchQuery),
+          },
+        }
+      );
 
-    if (response.status === 200) {
-      // Check if the response contains an 'items' array
-      if (response.data.items && Array.isArray(response.data.items)) {
-        // Extract image results from the data (modify this according to the API response structure)
-        const imageResults = response.data.items.filter(
-          (item) => item.pagemap && item.pagemap.cse_image
-        );
+      if (response.status === 200) {
+        // Check if the response contains an 'items' array
+        if (response.data.items && Array.isArray(response.data.items)) {
+          // Extract image results from the data
+          const imageResults = response.data.items.map((item) => ({
+            src: item.pagemap.cse_image[0].src,
+            alt: item.title,
+          }));
 
-        setResults(imageResults);
+          setResults(imageResults); // Update the results state with imageResults
+        } else {
+          throw new Error("Invalid response data format");
+        }
       } else {
-        throw new Error("Invalid response data format");
+        throw new Error(`Request failed with status code ${response.status}`);
       }
-    } else {
-      throw new Error(`Request failed with status code ${response.status}`);
+    } catch (error) {
+      console.error("Error:", error);
+      setError(
+        "An error occurred while fetching data. Please try again later."
+      );
     }
-  } catch (error) {
-    console.error("Error:", error);
-    setError("An error occurred while fetching data. Please try again later.");
-  }
-};
+  };
 
+  // Added this function to handle empty results
+  const handleEmptyResults = () => {
+    if (results.length === 0) {
+      return <p className="text-gray-500">No results found.</p>;
+    }
+  };
 
   return (
     <div>
@@ -64,9 +77,11 @@ const handleSearch = async () => {
       </div>
       {error && <p className="text-red-500">{error}</p>}
       <div className="container mx-auto p-4 grid grid-cols-2 sm:grid-cols-4 gap-4">
-        {results.map((photo, index) => (
-          <PhotoCard key={index} photo={photo} />
-        ))}
+        {results.length > 0 &&
+          results.map((photo, index) => (
+            <PhotoCard key={index} photo={photo} />
+          ))}
+        {handleEmptyResults()}
       </div>
     </div>
   );
